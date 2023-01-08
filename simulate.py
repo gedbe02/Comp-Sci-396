87ffc58731b14f752ff7859216fc3a663b83b267
gedbe02 <bengeduld2023@u.northwestern.edu>
2023-01-08 11:32:55 -0600

    Step 6

--- simulate.py
+++ simulate.py
@@ -3,10 +3,12 @@ import time
 
 physicsClient = p.connect(p.GUI)
 
+p.setGravity(0,0,-9.8)
 p.loadSDF("box.sdf")
+
 for i in range(1000):
     p.stepSimulation()
     print(i)
     time.sleep(1/60)
-    
+
 p.disconnect()
