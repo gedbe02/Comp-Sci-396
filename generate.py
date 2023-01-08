b2cfd504866c8e40a8ecec5a0b15f47ca1978f8d
gedbe02 <bengeduld2023@u.northwestern.edu>
2023-01-08 12:54:40 -0600

    Task D

--- generate.py
+++ generate.py
@@ -1,15 +1,30 @@
 import pyrosim.pyrosim as pyrosim
 
 length = 1
-width = 2
-height = 3
+width = 1
+height = 1
 
 x = 0
 y = 0
-z = 1.5
+z = 0.5
 
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+pyrosim.Start_SDF("boxes.sdf")
+
+for row in range(5):
+    for col in range(5):
+        for i in range(10):
+            pyrosim.Send_Cube(name="Box"+str(i)+"_"+str("row")+"_"+str(col), pos=[x,y,z] , size=[length,width,height])
+            z += 1
+            length *= .9
+            width *= .9
+            height *= .9
+        y+=1
+        z=0.5
+        length,width,height = 1,1,1
+    x+=1
+    y=0
+    z=0.5
+
 pyrosim.End()
 
 
