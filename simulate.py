3e6245ae340290d3eadb3a2d260f352751db2c1b
gedbe02 <bengeduld2023@u.northwestern.edu>
2023-01-08 11:26:25 -0600

    Task C

--- simulate.py
+++ simulate.py
@@ -2,8 +2,11 @@ import pybullet as p
 import time
 
 physicsClient = p.connect(p.GUI)
+
+p.loadSDF("box.sdf")
 for i in range(1000):
     p.stepSimulation()
     print(i)
     time.sleep(1/60)
+    
 p.disconnect()
