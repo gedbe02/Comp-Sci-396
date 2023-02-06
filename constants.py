4ad5a92badd1780880995dcfd45365ebafc8bf34
gedbe02 <bengeduld2023@u.northwestern.edu>
2023-02-05 19:26:45 -0600

    Added main.py

--- constants.py
+++ constants.py
@@ -25,8 +25,8 @@ fl_frequency = 10
 fl_phaseOffset = np.pi/4
 
 # Hill Climber
-numberOfGenerations = 0
-populationSize = 1
+numberOfGenerations = 10
+populationSize = 10
 numSensorNeurons = 6
 numMotorNeurons = 10
 motorJointRange = 1.5
