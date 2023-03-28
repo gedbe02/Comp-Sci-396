7aa20120c52c5d35bbca43aafeb7d21aca1f2ef0
gedbe02 <bengeduld2023@u.northwestern.edu>
2023-03-28 15:30:29 -0500

    Random

--- testing.py
+++ testing.py
@@ -1,4 +1,7 @@
 import os
 import numpy as np
-os.system("python3 simulate.py " + "GUI" + " "+ str(713) + " test testABot &")
+os.system("python3 simulate.py " + "GUI" + " "+ str(674) + " test testABot &")
 
+#1642
+#2832
+#674
